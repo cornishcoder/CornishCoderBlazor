cdd3fe19b867e9b350a37291e24f0817dfabaa26
Richard Keast <richard.keast@bluefruit.co.uk>
2018-06-21 07:18:40 +0100

    Added Targetprocess page to use API to get features.  WIP- js interop weirdness going on

---
+++ CornishCoderBlazor/wwwroot/jsonp.js
@@ -0,0 +1,39 @@
+﻿Blazor.registerFunction('jsUpdateTitle', function () {
+    const assemblyName = 'CornishCoderBlazor';
+    const namespace = 'CornishCoderBlazor.Pages';
+    const typeName = 'TargetprocessProject';
+    const updateTitle = Blazor.platform.findMethod(
+        assemblyName,
+        namespace,
+        typeName,
+        'UpdateTitle'
+    );
+    Blazor.platform.callMethod(updateTitle, null, [Blazor.platform.toDotNetString('message from jsonP js function!')]);
+});
+
+Blazor.registerFunction('jsonP', function (url) {
+    
+    var data = {};
+
+    var callback = function (data) {
+        const assemblyName = 'CornishCoderBlazor';
+        const namespace = 'CornishCoderBlazor.Pages';
+        const typeName = 'TargetprocessProject';
+        const methodName = 'ApiCallback';
+
+        const apiCallback = Blazor.platform.findMethod(
+            assemblyName,
+            namespace,
+            typeName,
+            methodName
+        );
+
+
+        console.log(data.Items);
+        //let stringy = Blazor.platform.toDotNetString(JSON.stringify(data));
+        
+        Blazor.platform.callMethod(apiCallback, null, [Blazor.platform.toDotNetString(JSON.stringify(data.Items))]);
+    };
+
+    J50Npi.getJSON(url, data, callback);
+});
